47aa5a0383f701150490bb0185b2e67b0ed7dc05
rafiulislamrony <rafiulislamrony23@gmail.com>
2023-11-04 16:00:43 +0600

    vue props and Props Validation

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,22 +1,45 @@
 <template>
-  <h1> {{ msg }}</h1>
-  
-  <contact-details> </contact-details>
+  <h1>{{ msg }}</h1>
 
+  <contact-details
+    v-for="contact in contacts"
+    :key="contact.name"
+    :name="contact.name"
+    :webAddress="contact.webAddress"
+    :address="contact.address"
+  >
+  </contact-details>
+ 
 </template>
 
 <script>
-import ContactDetails from './ContactDetails.vue'
+import ContactDetails from "./ContactDetails.vue";
 
 export default {
-  data(){
-      return{
-          msg: "Welllcome to my website"
-      };
-  }, 
-  components:{
-    ContactDetails
-  }
-}
-
+  data() {
+    return {
+      msg: "Welllcome to my website",
+      contacts: [
+        {
+          name: "sheab",
+          webAddress: "rrr.com",
+          address: "Natore",
+        },
+        {
+          name: "aka",
+          webAddress: "aka.com",
+          address: "Bonpara",
+        },
+        {
+          name: "Tisha",
+          webAddress: "tisha.com",
+          address: "Pabna",
+        },
+      ],
+    };
+  },
+  components: {
+    ContactDetails,
+  },
+};
 </script>
--- vue3/src/ContactDetails.vue
+++ vue3/src/ContactDetails.vue
@@ -1,20 +1,45 @@
 <template>
-    <h1> Contact Details</h1>
-    <p> <strong> Name:</strong>{{ name }}</p> 
-    <p> <strong> Website:</strong>{{ web }} </p> 
-    <p> <strong> Address:</strong> {{ address }} </p> 
+    <div class="contact-details">
+        <h1> Contact Details</h1>
+        <p> <strong> Name:</strong>{{ name }}</p>
+        <p> <strong> Website:</strong>{{ webAddress }} </p>
+        <p> <strong> Address:</strong> {{ address }} </p>
+    </div>
 </template>
 
 <script>
 
 export default {
-    data(){
-        return{
-            name: "Rafiul Islam",
-            web: "www.rafiul.com",
-            address: "Dhaka, Bangladesh"
+    props: {"name": {
+        type: String, 
+    }, "webAddress": {
+        type: String,
+        required: false,
+        default: "ddd.com"
+    }, "address": {
+        type: String,
+        required: true,
+        defoult: "ddd.com"
+    }},
+    data() {
+        return { 
         };
     },
 }
- 
+
 </script>
+
+<style>
+.contact-details {
+    border: 1px solid #ddd;
+    padding: 20px;
+    display: inline-block;
+    margin: 30px;
+}
+
+.contact-details h1{
+    color: blue; 
+}
+
+
+</style>

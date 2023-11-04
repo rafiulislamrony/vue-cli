4f44bb73c8bca3029d161bcab369502da4c7cec0
rafiulislamrony <rafiulislamrony23@gmail.com>
2023-11-04 16:45:07 +0600

    Component Slots

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,45 +1,49 @@
 <template>
   <h1>{{ msg }}</h1>
+ 
+    <my-card cardTitle="About Me" >
+      <p>Hi its me Rafiul Islam.....</p> 
+    </my-card>  
+    <my-card cardTitle="iphone 12"> 
+      <img src="https://static-01.daraz.com.bd/p/4067a01b45caf7e0eb593a884b50dd35.jpg_300x0q75.webp" alt="">
+    <p>
+      hi mdsh jf jhgbvd njsg jgdbsfds
+    </p>
 
-  <contact-details
-    v-for="contact in contacts"
-    :key="contact.name"
-    :name="contact.name"
-    :webAddress="contact.webAddress"
-    :address="contact.address"
-  >
-  </contact-details>
+    </my-card>  
+    <my-card cardTitle="service"> </my-card>    
  
 </template>
 
-<script>
-import ContactDetails from "./ContactDetails.vue";
+<script> 
+import MyCard from "./MyCard.vue";
 
 export default {
   data() {
     return {
-      msg: "Welllcome to my website",
-      contacts: [
-        {
-          name: "sheab",
-          webAddress: "rrr.com",
-          address: "Natore",
-        },
-        {
-          name: "aka",
-          webAddress: "aka.com",
-          address: "Bonpara",
-        },
-        {
-          name: "Tisha",
-          webAddress: "tisha.com",
-          address: "Pabna",
-        },
-      ],
+      msg: "Welllcome to my website", 
     };
   },
-  components: {
-    ContactDetails,
+  components: { 
+    MyCard 
   },
 };
+
 </script>
+
+
+
+
+
+<style>
+*{
+  margin: 0;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: sans-serif;
+  line-height: 1.8;
+} 
+</style>

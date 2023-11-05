591cd48ba4d0b6f27ae29c5e5ce242ab9336019d
rafiulislamrony <rafiulislamrony23@gmail.com>
2023-11-05 22:09:32 +0600

    exercise

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,49 +1,45 @@
 <template>
   <h1>{{ msg }}</h1>
- 
-    <my-card cardTitle="About Me" >
-      <p>Hi its me Rafiul Islam.....</p> 
-    </my-card>  
-    <my-card cardTitle="iphone 12"> 
-      <img src="https://static-01.daraz.com.bd/p/4067a01b45caf7e0eb593a884b50dd35.jpg_300x0q75.webp" alt="">
-    <p>
-      hi mdsh jf jhgbvd njsg jgdbsfds
-    </p>
 
-    </my-card>  
-    <my-card cardTitle="service"> </my-card>    
- 
+  <my-card cardTitle="About Me">
+    <p>Hi its me Rafiul Islam.....</p>
+  </my-card>
+
+  <my-card cardTitle="iphone 12">
+    <img
+      src="https://static-01.daraz.com.bd/p/4067a01b45caf7e0eb593a884b50dd35.jpg_300x0q75.webp"
+      alt="" />
+    <p>hi mdsh jf jhgbvd njsg jgdbsfds</p>
+  </my-card>
+
+  <my-card cardTitle="service"> </my-card>
+
 </template>
 
-<script> 
+<script>
 import MyCard from "./MyCard.vue";
 
 export default {
   data() {
     return {
-      msg: "Welllcome to my website", 
+      msg: "Welllcome to my website",
     };
   },
-  components: { 
-    MyCard 
+  components: {
+    MyCard,
   },
 };
-
 </script>
 
-
-
-
-
 <style>
-*{
+* {
   margin: 0;
   padding: 0px;
   box-sizing: border-box;
 }
 
-body{
+body {
   font-family: sans-serif;
   line-height: 1.8;
-} 
+}
 </style>
--- vue3/src/MyCard.vue
+++ vue3/src/MyCard.vue
@@ -1,43 +1,40 @@
 <template>
-    <div class="MyCard">
-        <div class="the-card_title">
-             {{ cardTitle }}
-        </div>
-        <div class="the-card_body">
-            <slot>
-                <p>This is defoult card content</p>
-            </slot>
-      
-        </div>
+  <div class="MyCard">
+    <div class="the-card_title">
+      {{ cardTitle }}
     </div>
+    <div class="the-card_body">
+      <slot>
+        <p>This is defoult card content</p>
+      </slot>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    props: ["cardTitle"]
-}
-
+  props: ["cardTitle"],
+};
 </script>
 
 <style>
- .MyCard{
-    width: 200px;
-    border: 1px solid #006f88;
-    min-height: 200px;
-    margin: 22px auto;
- }
- .the-card_title {
-    background: #167db1;
-    padding: 5px 11px;
-    color: #fff;
- }
+.MyCard {
+  width: 200px;
+  border: 1px solid #006f88;
+  min-height: 200px;
+  margin: 22px auto;
+}
+.the-card_title {
+  background: #167db1;
+  padding: 5px 11px;
+  color: #fff;
+}
 
- .the-card_body {
-    padding: 11px;
- }
+.the-card_body {
+  padding: 11px;
+}
 
- .the-card_body img{
-    height: 111px;
- } 
+.the-card_body img {
+  height: 111px;
+}
 </style>
